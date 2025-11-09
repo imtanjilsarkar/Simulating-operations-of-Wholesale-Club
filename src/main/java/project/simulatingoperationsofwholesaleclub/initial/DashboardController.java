5d10bb3b473b340f2c614083cfd4b28dc42fb2bf
imtanjilsarkar <im.tanjilsarkar@gmail.com>
2025-11-09 18:31:08 +0600

    add forgot password & dashboard fxml and controller part also make sceneBuilder

--- src/main/java/project/simulatingoperationsofwholesaleclub/initial/DashboardController.java
+++ src/main/java/project/simulatingoperationsofwholesaleclub/initial/DashboardController.java
@@ -1,5 +1,26 @@
 package project.simulatingoperationsofwholesaleclub.initial;
 
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class DashboardController {
+    @javafx.fxml.FXML
+    private AnchorPane contentArea;
+
+    @javafx.fxml.FXML
+    public void handleEmployee(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleSupport(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleLogout(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleReports(ActionEvent actionEvent) {
+    }
     //common dashboard for scene switching
 }
--- src/main/java/project/simulatingoperationsofwholesaleclub/initial/ForgotPasswordController.java
+++ src/main/java/project/simulatingoperationsofwholesaleclub/initial/ForgotPasswordController.java
@@ -1,5 +1,23 @@
 package project.simulatingoperationsofwholesaleclub.initial;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class ForgotPasswordController {
+    @javafx.fxml.FXML
+    private PasswordField newPasswordField;
+    @javafx.fxml.FXML
+    private TextField emailField;
+    @javafx.fxml.FXML
+    private PasswordField confirmPasswordField;
+
+    @javafx.fxml.FXML
+    public void handleResetPassword(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleBackToLogin(ActionEvent actionEvent) {
+    }
     //handles password recovery
 }
