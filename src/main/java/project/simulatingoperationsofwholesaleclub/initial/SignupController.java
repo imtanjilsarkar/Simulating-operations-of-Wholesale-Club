694ecb2afb60448ba2a64fef813d37a91ba71991
imtanjilsarkar <im.tanjilsarkar@gmail.com>
2025-11-09 10:18:36 +0600

    add singUp fxml and signUP controller part also make sceneBuilder

--- src/main/java/project/simulatingoperationsofwholesaleclub/initial/SignupController.java
+++ src/main/java/project/simulatingoperationsofwholesaleclub/initial/SignupController.java
@@ -1,5 +1,14 @@
 package project.simulatingoperationsofwholesaleclub.initial;
 
+import javafx.event.ActionEvent;
+
 public class SignupController {
+    @javafx.fxml.FXML
+    public void handleBack(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleSignUp(ActionEvent actionEvent) {
+    }
     //handles new user registration
 }
