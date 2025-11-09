21a4c43d96639402197592a94d2611c389a0d6d9
imtanjilsarkar <im.tanjilsarkar@gmail.com>
2025-11-09 20:49:07 +0600

    HR Manager SceneBuilder Work is completed now

--- src/main/java/project/simulatingoperationsofwholesaleclub/tanjil/hrManager/controller/ShiftController.java
+++ src/main/java/project/simulatingoperationsofwholesaleclub/tanjil/hrManager/controller/ShiftController.java
@@ -1,5 +1,24 @@
 package project.simulatingoperationsofwholesaleclub.tanjil.hrManager.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class ShiftController {
+    @javafx.fxml.FXML
+    private ComboBox employeeComboBox;
+    @javafx.fxml.FXML
+    private ComboBox <String> shiftComboBox;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+        shiftComboBox.getItems().addAll("Morning", "Evening", "Night");
+
+    }
+
+    @javafx.fxml.FXML
+    public void handleAssignShift(ActionEvent actionEvent) {
+    }
     //assign work shifts
 }
