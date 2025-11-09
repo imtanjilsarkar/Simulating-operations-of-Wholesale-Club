406384a006f2f4e86c9d4ea65324d50f09398a2f
imtanjilsarkar <im.tanjilsarkar@gmail.com>
2025-11-09 10:10:35 +0600

    add initial fxml and controller part also make sceneBuilder

--- src/main/java/project/simulatingoperationsofwholesaleclub/initial/InitialController.java
+++ src/main/java/project/simulatingoperationsofwholesaleclub/initial/InitialController.java
@@ -1,5 +1,14 @@
 package project.simulatingoperationsofwholesaleclub.initial;
 
+import javafx.event.ActionEvent;
+
 public class InitialController {
+    @javafx.fxml.FXML
+    public void handleLogin(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleSignup(ActionEvent actionEvent) {
+    }
     //main entry (login page)
 }
