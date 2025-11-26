2d1d28b67bc7ab50114af949822f3fb9bf1298c2
Fiu000 <nafiuislam111@gmail.com>
2025-11-26 22:51:48 +0600

    naming Inventory controller

---
+++ src/main/java/com/group16/simulatingoperationsofwholesaleclub/nafiu/Inventory/Controller/Monitor_Stock_Levels.java
@@ -0,0 +1,4 @@
+package com.group16.simulatingoperationsofwholesaleclub.nafiu.Inventory.Controller;
+
+public class Monitor_Stock_Levels {
+}
---
+++ src/main/java/com/group16/simulatingoperationsofwholesaleclub/nafiu/Inventory/Controller/Receive_Supplier_Delivery.java
@@ -0,0 +1,4 @@
+package com.group16.simulatingoperationsofwholesaleclub.nafiu.Inventory.Controller;
+
+public class Receive_Supplier_Delivery {
+}
