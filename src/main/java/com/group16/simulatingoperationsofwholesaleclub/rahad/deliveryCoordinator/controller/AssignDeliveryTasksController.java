ebd3c8270bbdfd6d58d91037e165a55e4f715991
rubayetrahman1-gif <rubayetrahman1@gmail.com>
2025-11-28 04:05:28 +0600

    desinged fxml for delivery controller goal 1

--- src/main/java/com/group16/simulatingoperationsofwholesaleclub/rahad/deliveryCoordinator/controller/AssignDeliveryTasksController.java
+++ src/main/java/com/group16/simulatingoperationsofwholesaleclub/rahad/deliveryCoordinator/controller/AssignDeliveryTasksController.java
@@ -1,4 +1,37 @@
 package com.group16.simulatingoperationsofwholesaleclub.rahad.deliveryCoordinator.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 public class AssignDeliveryTasksController {
+    @javafx.fxml.FXML
+    private TableColumn customerNameCol;
+    @javafx.fxml.FXML
+    private TableColumn quantityCol;
+    @javafx.fxml.FXML
+    private ComboBox staffComboBox;
+    @javafx.fxml.FXML
+    private TableView ordersTable;
+    @javafx.fxml.FXML
+    private TableColumn productListCol;
+    @javafx.fxml.FXML
+    private Label statusLabel;
+    @javafx.fxml.FXML
+    private TableColumn orderIdCol;
+    @javafx.fxml.FXML
+    private Button assignButton;
+    @javafx.fxml.FXML
+    private Button viewOrdersButton;
+
+    @Deprecated
+    public void assigndelivery(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ViewOrdersBTN(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void AssignDeliveryBTN(ActionEvent actionEvent) {
+    }
 }
