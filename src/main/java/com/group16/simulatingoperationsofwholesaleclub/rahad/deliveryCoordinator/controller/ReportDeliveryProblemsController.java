bda96afdf534ea74fa9c16b767e715c104855447
rubayetrahman1-gif <rubayetrahman1@gmail.com>
2025-11-29 00:04:17 +0600

    worked in scenebuilder design in user 5 goal 4

--- src/main/java/com/group16/simulatingoperationsofwholesaleclub/rahad/deliveryCoordinator/controller/ReportDeliveryProblemsController.java
+++ src/main/java/com/group16/simulatingoperationsofwholesaleclub/rahad/deliveryCoordinator/controller/ReportDeliveryProblemsController.java
@@ -1,4 +1,37 @@
 package com.group16.simulatingoperationsofwholesaleclub.rahad.deliveryCoordinator.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class ReportDeliveryProblemsController {
+
+    @javafx.fxml.FXML
+    private TextField orderIdField;
+    @javafx.fxml.FXML
+    private Label statusLabel;
+    @javafx.fxml.FXML
+    private TextArea problemDetailsTA;
+
+    @javafx.fxml.FXML
+    public void handleBack(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleSubmitProblem(ActionEvent actionEvent) {
+    }
+
+    @Deprecated
+    public void submitProblemBTN(ActionEvent actionEvent) {
+    }
+
+    @Deprecated
+    public void loadOrdersBTN(ActionEvent actionEvent) {
+    }
+
+    @Deprecated
+    public void handleReportIssue(ActionEvent actionEvent) {
+    }
 }
