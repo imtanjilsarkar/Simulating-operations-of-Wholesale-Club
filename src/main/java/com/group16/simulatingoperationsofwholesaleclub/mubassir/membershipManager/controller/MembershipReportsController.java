3db672799798369431e3e56de63cd689384c6297
Mubassir-2003 <m.m.ishmam@gmail.com>
2025-11-28 22:37:35 +0600

    Remaining 4 goals of membership manager scene design is done.

--- src/main/java/com/group16/simulatingoperationsofwholesaleclub/mubassir/membershipManager/controller/MembershipReportsController.java
+++ src/main/java/com/group16/simulatingoperationsofwholesaleclub/mubassir/membershipManager/controller/MembershipReportsController.java
@@ -1,4 +1,22 @@
 package com.group16.simulatingoperationsofwholesaleclub.mubassir.membershipManager.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+
 public class MembershipReportsController {
+    @javafx.fxml.FXML
+    private DatePicker toDP;
+    @javafx.fxml.FXML
+    private TextArea reportTA;
+    @javafx.fxml.FXML
+    private DatePicker fromDP;
+
+    @javafx.fxml.FXML
+    public void BackBTN(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ShowReportBTN(ActionEvent actionEvent) {
+    }
 }
--- src/main/java/com/group16/simulatingoperationsofwholesaleclub/mubassir/membershipManager/controller/NewMembershipController.java
+++ src/main/java/com/group16/simulatingoperationsofwholesaleclub/mubassir/membershipManager/controller/NewMembershipController.java
@@ -1,4 +1,25 @@
 package com.group16.simulatingoperationsofwholesaleclub.mubassir.membershipManager.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
 public class NewMembershipController {
+    @javafx.fxml.FXML
+    private TextField phoneTF;
+    @javafx.fxml.FXML
+    private DatePicker startDP;
+    @javafx.fxml.FXML
+    private ComboBox membershipTypeCB;
+    @javafx.fxml.FXML
+    private TextField customerNameTF;
+
+    @javafx.fxml.FXML
+    public void BackBTN(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void RegisterBTN(ActionEvent actionEvent) {
+    }
 }
