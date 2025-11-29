df4fd5957d064705646a1d97e0885b38c06b60c7
Fiu000 <nafiuislam111@gmail.com>
2025-11-29 23:15:27 +0600

    nafiu_goal7and8fxmldesign_29nov

--- src/main/java/com/group16/simulatingoperationsofwholesaleclub/nafiu/Cashier/Controller/Flag_Suspicious_Activity.java
+++ src/main/java/com/group16/simulatingoperationsofwholesaleclub/nafiu/Cashier/Controller/Flag_Suspicious_Activity.java
@@ -1,4 +1,26 @@
 package com.group16.simulatingoperationsofwholesaleclub.nafiu.Cashier.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
 public class Flag_Suspicious_Activity {
+    @javafx.fxml.FXML
+    private Label recordStatusLabel;
+    @javafx.fxml.FXML
+    private Label confirmationLabel;
+    @javafx.fxml.FXML
+    private TextArea reasonField;
+
+    @javafx.fxml.FXML
+    public void handleSubmitReport(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleReportTransaction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleBack(ActionEvent actionEvent) {
+    }
 }
--- src/main/java/com/group16/simulatingoperationsofwholesaleclub/nafiu/Cashier/Controller/Queue_Management.java
+++ src/main/java/com/group16/simulatingoperationsofwholesaleclub/nafiu/Cashier/Controller/Queue_Management.java
@@ -1,4 +1,19 @@
 package com.group16.simulatingoperationsofwholesaleclub.nafiu.Cashier.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
 public class Queue_Management {
+    @javafx.fxml.FXML
+    private Label readyStatusLabel;
+    @javafx.fxml.FXML
+    private Label transactionLogLabel;
+
+    @javafx.fxml.FXML
+    public void handleBack(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleNextCustomer(ActionEvent actionEvent) {
+    }
 }
