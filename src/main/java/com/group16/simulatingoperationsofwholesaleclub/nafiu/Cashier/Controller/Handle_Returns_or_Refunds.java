dc1fcc6654d5c2aa38fb16a757aee1ba2a2c9ec9
Fiu000 <nafiuislam111@gmail.com>
2025-11-29 22:26:52 +0600

    nafiu_goal3and4fxmldesign_29nov

--- src/main/java/com/group16/simulatingoperationsofwholesaleclub/nafiu/Cashier/Controller/Handle_Returns_or_Refunds.java
+++ src/main/java/com/group16/simulatingoperationsofwholesaleclub/nafiu/Cashier/Controller/Handle_Returns_or_Refunds.java
@@ -1,4 +1,30 @@
 package com.group16.simulatingoperationsofwholesaleclub.nafiu.Cashier.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class Handle_Returns_or_Refunds {
+    @javafx.fxml.FXML
+    private Label refundAmountLabel;
+    @javafx.fxml.FXML
+    private TextField productIdField;
+    @javafx.fxml.FXML
+    private TextField billNoField;
+    @javafx.fxml.FXML
+    private Label refundConfirmationLabel;
+    @javafx.fxml.FXML
+    private Label eligibilityLabel;
+
+    @javafx.fxml.FXML
+    public void handleVerifyReturnItem(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleBack(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleSelectReturnItem(ActionEvent actionEvent) {
+    }
 }
--- src/main/java/com/group16/simulatingoperationsofwholesaleclub/nafiu/Cashier/Controller/Process_Payment.java
+++ src/main/java/com/group16/simulatingoperationsofwholesaleclub/nafiu/Cashier/Controller/Process_Payment.java
@@ -1,4 +1,31 @@
 package com.group16.simulatingoperationsofwholesaleclub.nafiu.Cashier.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class Process_Payment {
+    @javafx.fxml.FXML
+    private ComboBox paymentMethodBox;
+    @javafx.fxml.FXML
+    private TextField paymentDetailsField;
+    @javafx.fxml.FXML
+    private Label validationLabel;
+    @javafx.fxml.FXML
+    private Label balanceLabel;
+    @javafx.fxml.FXML
+    private Label paymentStatusLabel;
+
+    @javafx.fxml.FXML
+    public void handleBack(ActionEvent actionEvent) {
+    }
+
+    @Deprecated
+    public void handleGenerateReceipt(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleSubmitPaymentInfo(ActionEvent actionEvent) {
+    }
 }
