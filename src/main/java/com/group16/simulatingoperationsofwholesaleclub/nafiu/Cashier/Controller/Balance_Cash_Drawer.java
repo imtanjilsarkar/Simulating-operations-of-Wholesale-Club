e9fbd06cb19af684d699fbdf2143fc483ca33e90
Fiu000 <nafiuislam111@gmail.com>
2025-11-29 22:42:12 +0600

    nafiu_goal5and6fxmldesign_29nov

--- src/main/java/com/group16/simulatingoperationsofwholesaleclub/nafiu/Cashier/Controller/Balance_Cash_Drawer.java
+++ src/main/java/com/group16/simulatingoperationsofwholesaleclub/nafiu/Cashier/Controller/Balance_Cash_Drawer.java
@@ -1,4 +1,28 @@
 package com.group16.simulatingoperationsofwholesaleclub.nafiu.Cashier.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class Balance_Cash_Drawer {
+    @javafx.fxml.FXML
+    private Label balanceStatusLabel;
+    @javafx.fxml.FXML
+    private Label comparisonLabel;
+    @javafx.fxml.FXML
+    private TextField actualCashField;
+    @javafx.fxml.FXML
+    private Label differenceLabel;
+
+    @javafx.fxml.FXML
+    public void handleBack(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleEndShift(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleSubmitCash(ActionEvent actionEvent) {
+    }
 }
--- src/main/java/com/group16/simulatingoperationsofwholesaleclub/nafiu/Cashier/Controller/Generate_Daily_Sales_Report.java
+++ src/main/java/com/group16/simulatingoperationsofwholesaleclub/nafiu/Cashier/Controller/Generate_Daily_Sales_Report.java
@@ -1,4 +1,21 @@
 package com.group16.simulatingoperationsofwholesaleclub.nafiu.Cashier.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
 public class Generate_Daily_Sales_Report {
+    @javafx.fxml.FXML
+    private Label reportStatusLabel;
+    @javafx.fxml.FXML
+    private Label netSalesLabel;
+    @javafx.fxml.FXML
+    private Label transactionDataLabel;
+
+    @javafx.fxml.FXML
+    public void handleBack(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleGenerateReport(ActionEvent actionEvent) {
+    }
 }
