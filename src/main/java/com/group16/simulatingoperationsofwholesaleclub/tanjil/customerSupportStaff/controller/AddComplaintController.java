c3bf8ccbedef349f2abf19749bc8c51c352fa5ab
imtanjilsarkar <im.tanjilsarkar@gmail.com>
2025-12-01 17:45:23 +0600

    Add Customer Staff two goal Add complaint and close complaint Logic also add two txt file and add a model class for user Complaint

--- src/main/java/com/group16/simulatingoperationsofwholesaleclub/tanjil/customerSupportStaff/controller/AddComplaintController.java
+++ src/main/java/com/group16/simulatingoperationsofwholesaleclub/tanjil/customerSupportStaff/controller/AddComplaintController.java
@@ -2,11 +2,14 @@ package com.group16.simulatingoperationsofwholesaleclub.tanjil.customerSupportSt
 
 import com.group16.simulatingoperationsofwholesaleclub.BaseController;
 import com.group16.simulatingoperationsofwholesaleclub.SceneSwitcher;
+import com.group16.simulatingoperationsofwholesaleclub.tanjil.customerSupportStaff.modelClass.Complaint;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class AddComplaintController extends BaseController {
@@ -21,6 +24,13 @@ public class AddComplaintController extends BaseController {
 
     @javafx.fxml.FXML
     public void handleCancel(ActionEvent actionEvent) {
+        customerNameField.clear();
+        complaintTitleField.clear();
+        descriptionArea.clear();
+
+        if (messageLabel != null) {
+            messageLabel.setText("");
+        }
     }
 
     @javafx.fxml.FXML
@@ -29,7 +39,26 @@ public class AddComplaintController extends BaseController {
         String complaintTitle = complaintTitleField.getText();
         String description = descriptionArea.getText();
 
-        messageLabel.setText("Complaint Add Successfully");
+        if(customerName.isEmpty() || complaintTitle.isEmpty() || description.isEmpty()) {
+            messageLabel.setText("Please fill all fields");
+            return;
+        }
+
+
+        Complaint complaint = new Complaint(customerName, complaintTitle, description);
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("complaints.txt", true))) {
+            writer.write(complaint.toString());
+            writer.newLine();
+            messageLabel.setText("Complaint added successfully!");
+        } catch (IOException e) {
+            messageLabel.setText("Error saving complaint");
+            e.printStackTrace();
+        }
+
+        customerNameField.clear();
+        complaintTitleField.clear();
+        descriptionArea.clear();
     }
 
 
