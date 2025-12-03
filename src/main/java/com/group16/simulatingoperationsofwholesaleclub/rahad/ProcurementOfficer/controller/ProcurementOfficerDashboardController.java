007ac3f39bc52b0b19b176beab3a4850a5ec5f3e
rubayetrahman1-gif <rubayetrahman1@gmail.com>
2025-12-03 11:17:03 +0600

    done scene switching from dashboard to other fxml for user 6

--- src/main/java/com/group16/simulatingoperationsofwholesaleclub/rahad/ProcurementOfficer/controller/ProcurementOfficerDashboardController.java
+++ src/main/java/com/group16/simulatingoperationsofwholesaleclub/rahad/ProcurementOfficer/controller/ProcurementOfficerDashboardController.java
@@ -6,9 +6,7 @@ import javafx.event.ActionEvent;
 import java.io.IOException;
 
 public class ProcurementOfficerDashboardController {
-    @javafx.fxml.FXML
-    public void createOrder(ActionEvent actionEvent) {
-    }
+
 
     @javafx.fxml.FXML
     public void handleBack(ActionEvent actionEvent) throws IOException {
@@ -16,30 +14,44 @@ public class ProcurementOfficerDashboardController {
     }
 
     @javafx.fxml.FXML
-    public void trackPendingDeliveries(ActionEvent actionEvent) {
+    public void trackPendingDeliveries(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchTo("/com/group16/simulatingoperationsofwholesaleclub/rahad/ProcurementOfficer/track_pen_del.fxml",actionEvent);
     }
 
     @javafx.fxml.FXML
-    public void generateReport(ActionEvent actionEvent) {
+    public void generateReport(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchTo("/com/group16/simulatingoperationsofwholesaleclub/rahad/ProcurementOfficer/generate_report.fxml",actionEvent);
     }
 
     @javafx.fxml.FXML
-    public void resolveIssues(ActionEvent actionEvent) {
+    public void resolveIssues(ActionEvent actionEvent) throws IOException{
+        SceneSwitcher.switchTo("/com/group16/simulatingoperationsofwholesaleclub/rahad/ProcurementOfficer/resolve_issue.fxml",actionEvent);
+
     }
 
     @javafx.fxml.FXML
-    public void contactSupplier(ActionEvent actionEvent) {
+    public void contactSupplier(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchTo("/com/group16/simulatingoperationsofwholesaleclub/rahad/ProcurementOfficer/view_supp_info.fxml",actionEvent);
     }
 
     @javafx.fxml.FXML
-    public void reviewRequests(ActionEvent actionEvent) {
+    public void manageRecords(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchTo("/com/group16/simulatingoperationsofwholesaleclub/rahad/ProcurementOfficer/manage_records.fxml",actionEvent);
     }
 
     @javafx.fxml.FXML
-    public void manageRecords(ActionEvent actionEvent) {
+    public void approveQuotation(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchTo("/com/group16/simulatingoperationsofwholesaleclub/rahad/ProcurementOfficer/approve_quotation.fxml",actionEvent);
     }
 
     @javafx.fxml.FXML
-    public void approveQuotation(ActionEvent actionEvent) {
+    public void reviewPurchaseRequests(ActionEvent actionEvent) throws IOException{
+        SceneSwitcher.switchTo("/com/group16/simulatingoperationsofwholesaleclub/rahad/ProcurementOfficer/purchase_req.fxml",actionEvent);
+    }
+
+    @javafx.fxml.FXML
+    public void createSupOrder(ActionEvent actionEvent) throws IOException {
+
+        SceneSwitcher.switchTo("/com/group16/simulatingoperationsofwholesaleclub/rahad/ProcurementOfficer/track_pen_del.fxml",actionEvent);
     }
 }
