2d7e7019a134d5b997cfafc7f065a669548db3dc
Fiu000 <nafiuislam111@gmail.com>
2025-12-05 15:07:19 +0600

    nafiu_suspiciousactivity.txt file update_05dec

--- src/main/java/com/group16/simulatingoperationsofwholesaleclub/nafiu/Cashier/Controller/Flag_Suspicious_Activity.java
+++ src/main/java/com/group16/simulatingoperationsofwholesaleclub/nafiu/Cashier/Controller/Flag_Suspicious_Activity.java
@@ -5,9 +5,12 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Flag_Suspicious_Activity {
+
     @javafx.fxml.FXML
     private Label recordStatusLabel;
     @javafx.fxml.FXML
@@ -17,20 +20,32 @@ public class Flag_Suspicious_Activity {
 
     @javafx.fxml.FXML
     public void handleSubmitReport(ActionEvent actionEvent) {
-        String reason = reasonField.getText();
 
-        if (reason == null || reason.trim().isEmpty()) {
+        String reason = reasonField.getText().trim();
+
+        if (reason.isEmpty()) {
             confirmationLabel.setText("Please enter a reason");
             return;
         }
-        recordStatusLabel.setText("Record Saved");
-        confirmationLabel.setText("Manager Notified");
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("suspiciousactivity.txt", true))) {
+            writer.write(reason);
+            writer.newLine();
+
+            recordStatusLabel.setText("Record Saved");
+            confirmationLabel.setText("Manager Notified");
+
+        } catch (IOException e) {
+            confirmationLabel.setText("Error saving report");
+            e.printStackTrace();
+        }
+
+
+        reasonField.clear();
     }
-
-
 
     @javafx.fxml.FXML
     public void handleBack(ActionEvent actionEvent) throws IOException {
-        SceneSwitcher.switchTo("/com/group16/simulatingoperationsofwholesaleclub/nafiu/Cashier/cashier_dashboard.fxml",actionEvent);
+        SceneSwitcher.switchTo("/com/group16/simulatingoperationsofwholesaleclub/nafiu/Cashier/cashier_dashboard.fxml", actionEvent);
     }
 }
